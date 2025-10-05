4e83e41886ba5c919a3befdf0a2dd86de6485038
AKD27 <dhiman27ayush@gmail.com>
2025-10-05 20:43:23 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+
+import Hero from "@/components/Hero";
+import { navItems } from "@/data";
+
+import Image from "next/image";
+import components from "@/components";
+
+const { Grid, RecentProject, Client, Experience, Approch, Footer,FloatingNav } = components;
+
+
+export default function Home() {
+  return (
+   <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden  sm:px-10 px-5 ">
+    <div className="w-full max-w-7xl">
+    <FloatingNav navItems={navItems} />
+        <Hero/>
+        <Grid/>
+        <RecentProject/>
+        <Client/>
+        <Experience/>
+        <Approch/>
+        <Footer/>
+    </div>
+
+   </main>
+  );
+}
